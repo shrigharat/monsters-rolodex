1d04a28f4b0c0f76a21d03938b95feb22325df5d
Shrishail Gharat <gharatshri014@gmail.com>
2020-08-10 22:45:49 +0530

    Updates

---
+++ precache-manifest.412c37f4dac33a3f7e7f3f852c844f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0bbff8d1e9eba576fea55be2b9df25",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f2ab0b6a94ed03b47b33",
+    "url": "/monsters-rolodex/static/css/main.9ca268de.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ab0b6a94ed03b47b33",
+    "url": "/monsters-rolodex/static/js/main.51315a08.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
